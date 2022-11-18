26b096ca489698a0883ed3315b2e90aa9f4fdf3b
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2022-11-18 22:00:54 +0800

    tests until posts

--- test.php
+++ test.php
@@ -28,23 +28,106 @@ DB::changeDatabase('testsocialmedia');
 
 var_dump(DB::getInstance());
 
+/**
+ * USER TESTING!!!!!!!!!!!!!!!!!!!!!!!
+ */
+echo "<details><summary>User testing!</summary>";
 $userHandler = new UserHandler();
-
 $user = new User();
+$time = time();
+$email = "{$time}@testing.com";
 
 $user
-    ->setEmail('test')
+    ->setEmail($email)
     ->setPassword('testpass')
-    ->setUsername('testusernasme')
-    ->setDisplayName('testdisplayname')
-    ->setBio('testbio');
-
+    ->setUsername($time)
+    ->setDisplayName("testdisplayname$time")
+    ->setBio("testbio$time")
+    ->setAvatar("$time.png");
 var_dump($user);
 
+echo "===============new user<br>";
 $userHandler->add($user);
 $lastUserId = $userHandler->lastInsertId();
-$user = $userHandler->getUser($lastUserId);
-var_dump($user);
-$userHandler->delete($user);
-$user = $userHandler->getUser($lastUserId);
-var_dump($user);
+$useradded = $userHandler->getUser($lastUserId);
+var_dump($useradded);
+
+echo "==============update<br>";
+$useradded->setAvatar('modified')->setUsername('modified')->setEmail('modified')->setDisplayName('modified')->setPassword('modified')->setBio('modified');
+var_dump($useradded);
+$u = $userHandler->update($useradded);
+if (!$u) {
+    var_dump($userHandler->getExecutionFeedback());
+}
+
+echo "===============delete<br>";
+$d = $userHandler->delete($useradded);
+if (!$d) {
+    var_dump($userHandler->getExecutionFeedback());
+}
+
+$userdeleted = $userHandler->getUser($email, 'email');
+var_dump($userdeleted);
+var_dump($userHandler->getUser($lastUserId));
+
+echo "===============purge<br>";
+$p = $userHandler->purge($userHandler->getUser($lastUserId));
+if (!$p) {
+    var_dump($userHandler->getExecutionFeedback());
+}
+var_dump($userHandler->getUser($lastUserId));
+echo "</details>";
+
+/**
+ * POST TESTING!!!!!!!!!!!!!!!!!!!!!!!
+ */
+echo "<details><summary>Post testing!</summary>";
+$postHandler = new PostHandler();
+$post = new Post();
+
+$post->setContent("post $time")->setUserId($time)->setRating($time);
+var_dump($post);
+
+echo "===============new post<br>";
+$np = $postHandler->add($post);
+if (!$np) {
+    echo ($postHandler->getExecutionFeedback());
+}
+$lastId = $postHandler->lastInsertId();
+$lastPost = $postHandler->getPost($lastId);
+var_dump($lastPost);
+
+echo "===============update<br>";
+$lastPost->setContent('modified')->setRating(0);
+$up = $postHandler->update($lastPost);
+if (!$up) {
+    echo ($postHandler->getExecutionFeedback());
+}
+$lastPost = $postHandler->getPost($lastId);
+var_dump($lastPost);
+
+echo "===============delete<br>";
+$dp = $postHandler->delete($lastPost);
+if (!$dp) {
+    echo ($postHandler->getExecutionFeedback());
+}
+$lastPost = $postHandler->getPost($lastId);
+var_dump($lastPost);
+
+echo "===============purge<br>";
+$pp = $postHandler->purge($lastPost);
+if (!$pp) {
+    var_dump($postHandler->getExecutionFeedback());
+}
+$lastPost = $postHandler->getPost($lastId);
+var_dump($lastPost);
+echo "</details>";
+
+echo "</details>";
+
+/**
+ * Comment TESTING!!!!!!!!!!!!!!!!!!!!!!!
+ */
+echo "<details><summary>Comment testing!</summary>";
+
+echo "</details>";
