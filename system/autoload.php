a5efedbaec373f1d1e2dad22552f5f0979f1d6f0
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2022-11-19 19:26:54 +0800

    php 8 autoload

--- system/autoload.php
+++ system/autoload.php
@@ -5,22 +5,24 @@ require "db.php";
 
 spl_autoload_register(function ($className) {
     // if class is in a namespace, exit
-    if (strpos($className, '/') !== false) return;
+    if (str_contains($className, '/')) return;
 
     // check if class has some suffix to them
-    if (strpos($className, 'DAO') !== false) {
+    if (str_contains($className, 'DAO')) {
         if (file_exists(__DIR__ . "/dao/$className.php")) {
             require_once __DIR__ . "/dao/$className.php";
         }
-    } elseif (strpos($class_name, 'Handler') !== false) {
+    } elseif (str_contains($class_name, 'Handler')) {
         if (file_exists(__DIR__ . "/handler/$className.php")) {
             require_once __DIR__ . "/handler/$className.php";
         }
-    } elseif (
-        strpos($class_name, 'Object') !== false
-    ) {
+    } elseif (str_contains($class_name, 'Object')) {
         if (file_exists(__DIR__ . "/object/$className.php")) {
             require_once __DIR__ . "/object/$className.php";
+        }
+    } else {
+        if (file_exists(__DIR__ . "/$className.php")) {
+            require_once __DIR__ . "/$className.php";
         }
     }
 });
