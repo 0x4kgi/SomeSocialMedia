d027d5a14170f5eca323fdb2c6f00982701bfa6e
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2022-11-25 19:54:05 +0800

    use dark boostrap for css

--- layouts/portal/base.php
+++ layouts/portal/base.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/picnic.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css" rel="stylesheet">
     <title>Portal</title>
 
     <style>
